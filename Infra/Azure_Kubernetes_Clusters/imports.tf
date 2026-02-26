51f05e14372abbc98c783be2da962cb008cbe78c
G. Kiran Kumar <gkirankumar@meilgroup2.onmicrosoft.com>
2026-02-26 15:06:07 +0530

    cognive resorces 6

--- Infra/Azure_Kubernetes_Clusters/imports.tf
+++ Infra/Azure_Kubernetes_Clusters/imports.tf
@@ -44,6 +44,6 @@ import {
   for_each = local.openai_models
 #  #id        = each.key
   to       = azurerm_cognitive_deployment.models[each.key]
-#  #id       ="/subscriptions/3baeba7a-1c88-4266-91ef-7a9738a6dfef/resourceGroups/${each.value.rg}/providers/Microsoft.CognitiveServices/accounts/${each.value.account}/deployments/${each.value.deployment_name}"
-  id       ="/subscriptions/3baeba7a-1c88-4266-91ef-7a9738a6dfef/resourceGroups/RG-AI-Service-Resources/providers/Microsoft.CognitiveServices/accounts/oai-service-account-01/deployments/${each.value.model_name}"
+#  #id       ="/subscriptions/3baeba7a-1c88-4266-91ef-7a9738a6dfef/resourceGroups/${each.value.rg}/providers/Microsoft.CognitiveServices/accounts/${each.value.account}/deployments/${each.value.model_name}"
+  id       ="/subscriptions/3baeba7a-1c88-4266-91ef-7a9738a6dfef/resourceGroups/RG-AI-Service-Resources/providers/Microsoft.CognitiveServices/accounts/oai-service-account-01/deployments/${each.value.deployment_name}"
 }
